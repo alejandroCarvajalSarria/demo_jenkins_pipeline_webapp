ca165a5d94aac3bc35ea765a4d4d968a7471de0c
Alejandro Carvajal <programmeralek@gmail.com>
2025-10-19 11:00:23 -0400

    V5 Webhook adding secret + extra container proxy on jenkins controller ec2 host

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (_req, res) => res.send('Hello from Alek v4 🚀'));
+app.get('/', (_req, res) => res.send('Hello from Alek v5 🚀'));
 app.listen(3000, () => console.log('listening on 3000'));
